de3307eab3c72c9495cee3481e4bed30445d746d
taojinbao <taojinbao@ouyeel.com>
2017-09-09 12:01:00 +0800

    update .gitignore

--- src/main/java/org/github/ttjkst/processes/TypeProcess.java
+++
@@ -1,9 +0,0 @@
-package org.github.ttjkst.processes;
-
-/**
- * Created by ttjkst on 2017/8/28.
- */
-public interface TypeProcess<T> {
-     T get(String src);
-     boolean support(Class<?> clazz);
-}
--- src/main/java/org/github/ttjkst/server/connector/proxy/ProxyFactory.java
+++
@@ -1,9 +0,0 @@
-package org.github.ttjkst.server.connector.proxy;
-
-/**
- * Created by ttjkst on 2017/9/5.
- */
-public interface ProxyFactory {
-    <T> T createProxy(Class<T> t);
-
-}
